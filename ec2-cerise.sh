0c8a65f519dddef939a3cf32e23a3e8a8db0f839
David Cako <kingcako@gmail.com>
2016-07-27 16:23:59 -0600

    nice meme

--- ec2-cerise.sh
+++ ec2-cerise.sh
@@ -3,7 +3,7 @@
 # ssh-security-group must be an existing security group that opens :22
 # this is necessary for scp and ssh access
 
-# wide-open must be an existing security group that allows ec2-run-instances
+# wide-open must be an existing IAM role that allows ec2-run-instances
 
 . conf/ec2-init.conf
 
@@ -13,6 +13,6 @@ sed -i "s/GIT_TOKEN = \"\"/GIT_TOKEN = \"$GIT_TOKEN\"/" master-ec2-init-tmp
 sed -i "s/SLAVE_PASS = \"\"/SLAVE_PASS = \"$SLAVE_PASS\"/" master-ec2-init-tmp  
  
 
-aws ec2-run-instances ami-d732f0b7 -t t2.micro -k $AWS_KEYPAIR -g ssh-security-group wide-open -f master-ec2-init-tmp
+aws ec2 run-instances --image-id ami-d732f0b7 --instance-type t2.micro --key-name $AWS_KEYPAIR --security-groups ssh-security-group --iam-instance-profile Name=wide-open --user-data master-ec2-init-tmp
 
 rm master-ec2-init-tmp 
