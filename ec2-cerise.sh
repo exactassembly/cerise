1ed5479bbe35d490b12215be3df97cbf4d50a7ac
David Cako <kingcako@gmail.com>
2016-07-27 15:48:05 -0600

    nice meme

--- ec2-cerise.sh
+++ ec2-cerise.sh
@@ -13,6 +13,6 @@ sed -i "s/GIT_TOKEN = \"\"/GIT_TOKEN = \"$GIT_TOKEN\"/" master-ec2-init-tmp
 sed -i "s/SLAVE_PASS = \"\"/SLAVE_PASS = \"$SLAVE_PASS\"/" master-ec2-init-tmp  
  
 
-ec2-run-instances ami-d732f0b7 -t t2.micro -k $AWS_KEYPAIR -g ssh-security-group wide-open -f master-ec2-init-tmp
+aws ec2-run-instances ami-d732f0b7 -t t2.micro -k $AWS_KEYPAIR -g ssh-security-group wide-open -f master-ec2-init-tmp
 
 rm master-ec2-init-tmp 
