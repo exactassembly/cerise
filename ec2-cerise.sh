6f9f81794894c38bc0ef2f3f37e83347b5d2563b
David Cako <kingcako@gmail.com>
2016-07-27 15:37:13 -0600

    add wide-open group

--- ec2-cerise.sh
+++ ec2-cerise.sh
@@ -1,7 +1,10 @@
 # awscli must be installed and configured with user auth
 
-# ssh-security-group is an existing security group that opens :22
+# ssh-security-group must be an existing security group that opens :22
 # this is necessary for scp and ssh access
+
+# wide-open must be an existing security group that allows ec2-run-instances
+
 . conf/ec2-init.conf
 
 cp ec2-init/master-ec2-init ./master-ec2-init-tmp
@@ -10,6 +13,6 @@ sed -i "s/GIT_TOKEN = \"\"/GIT_TOKEN = \"$GIT_TOKEN\"/" master-ec2-init-tmp
 sed -i "s/SLAVE_PASS = \"\"/SLAVE_PASS = \"$SLAVE_PASS\"/" master-ec2-init-tmp  
  
 
-ec2-run-instances ami-d732f0b7 -t t2.micro -k $AWS_KEYPAIR -g ssh-security-group -f master-ec2-init-tmp
+ec2-run-instances ami-d732f0b7 -t t2.micro -k $AWS_KEYPAIR -g ssh-security-group wide-open -f master-ec2-init-tmp
 
 rm master-ec2-init-tmp 
