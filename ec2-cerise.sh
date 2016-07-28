a2fdeefe9635b349d71925b8dca0d9d39eadfe7c
David Cako <kingcako@gmail.com>
2016-07-28 16:30:09 -0600

    added verbosity and exception handling

--- ec2-cerise.sh
+++ ec2-cerise.sh
@@ -3,9 +3,21 @@
 # "ssh-security-group" must be an existing security group that opens :22
 # "wide-open" must be an existing IAM role that allows ec2-run-instances
 
-# "--master-only" may be passed as an argument to forego creating a new slave image
-
 . conf/ec2-init.conf
+
+# verify ec2-init.conf
+if [ ! "$GIT_TOKEN" ] || [ ! "$MASTER_ADDRESS" ] || [ ! "SLAVE_PASS"]; then
+    echo "Missing variable(s) in conf/ec2.init.conf."
+    exit 1
+fi 
+
+aws sts get-caller-identity --output table
+
+if [ $? != 0 ]
+    exit 1
+elif
+    echo "Valid awscli configuration found!"
+fi
 
 cp ec2-init/master-ec2-init ./master-ec2-init-tmp
 
@@ -13,31 +25,42 @@ sed -i "s/GIT_TOKEN=\"\"/GIT_TOKEN=\"$GIT_TOKEN\"/" master-ec2-init-tmp
 sed -i "s/SLAVE_PASS=\"\"/SLAVE_PASS=\"$SLAVE_PASS\"/" master-ec2-init-tmp  
 
 # spin up master instance, querying for InstanceId and assigning to variable "MASTER_ID"
-MASTER_ID=$(aws ec2 run-instances --image-id ami-d732f0b7 --instance-type t2.micro --key-name david-kp \
---security-groups ssh-security-group --iam-instance-profile Name=wide-open --user-data master-ec2-init-tmp --query 'Instances[0].InstanceId')
-# remove quotes
-MASTER_ID=$(sed 's/\"//g' <<< $MASTER_ID)
+echo "Spinning up master instance..."
+MASTER_ID=$(aws ec2 run-instances --image-id ami-d732f0b7 --instance-type t2.micro --key-name $AWS_KEYPAIR \
+--security-groups ssh-security-group --iam-instance-profile Name=wide-open --user-data master-ec2-init-tmp --query 'Instances[0].InstanceId' --output text)
 
 # output of run-instances apparently comes a fraction of a second before an IP address is assigned
 # such a usability
-MASTER_ADDRESS=$(aws ec2 describe-instances --instance-id $MASTER_ID --query 'Instances[0].PublicIpAddress')
-MASTER_ADDRESS=$(sed 's/\"//g' <<< $MASTER_ADDRESS)
-
+MASTER_ADDRESS=$(aws ec2 describe-instances --instance-id $MASTER_ID --query 'Instances[0].PublicIpAddress' --output text)
+echo "Master IP address = " $MASTER_ADDRESS
 
 rm master-ec2-init-tmp 
 
-# begin spinup of slave with master IP address in-hand
+echo "Checking for existing slave AMI..."
+SLAVE_EXISTS=$(aws ec2 describe-images --filters "Name=owner-id,Values=$(aws sts get-caller-identity --query Account --output text)" "Name=name,Values=SLAVE_AMI" --query Images[0].ImageId --output text)
 
-if [ $1 != "--master-only" ]; then 
+# begin spinup of slave with master IP address in-hand
+if [ "$SLAVE_EXISTS" = "None" ]; then 
     cp ec2-init/slave-ec2-init ./slave-ec2-init-tmp
 
     sed -i "s/GIT_TOKEN=\"\"/GIT_TOKEN=\"$GIT_TOKEN\"/" slave-ec2-init-tmp
     sed -i "s/MASTER_ADDRESS=\"\"/MASTER_ADDRESS=\"$MASTER_ADDRESS\"/" slave-ec2-init-tmp
     sed -i "s/SLAVE_PASS=\"\"/SLAVE_PASS=\"$SLAVE_PASS\"/" slave-ec2-init-tmp
 
+    echo "No existing AMI. Spinning up dummy slave instance..."
     SLAVE_ID=$(aws ec2 run-instances --image-id ami-d732f0b7 --instance-type t2.micro --key-name $AWS_KEYPAIR \
-    --security-groups ssh-security-group --user-data slave-ec2-init-tmp --query 'Instances[0].InstanceId')
-    SLAVE_ID=$(sed 's/\"//g' <<< $SLAVE_ID)
+    --security-groups ssh-security-group --user-data slave-ec2-init-tmp --query 'Instances[0].InstanceId' --output text)
 
+    echo "Waiting for slave init to complete..."
+    aws ec2 wait instance-status-ok --instance-ids $SLAVE_ID
+    echo "Commiting slave instance to image..." 
     aws ec2 create-image --instance-id $SLAVE_ID --name="SLAVE_AMI"
+    echo "Waiting for image (this will take time)..."
+    aws ec2 wait image-available --filters "Name=owner-id,Values=$(aws sts get-caller-identity --query Account --output text)" "Name=name,Values=SLAVE_AMI"
+    echo "Terminating dummy slave..."
+    aws ec2 terminate-instances --instance-ids $SLAVE_ID
+else
+    echo "Slave image already exists.  Omitting slave spinup."
 fi
+
+echo "All tasks complete."
