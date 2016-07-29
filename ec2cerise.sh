e3b8229ee4a16e3f8b22595ee2e789e52227161f
David Cako <kingcako@gmail.com>
2016-07-29 13:06:29 -0600

    new new ssh tail

--- ec2cerise.sh
+++ ec2cerise.sh
@@ -61,7 +61,7 @@ if [ "$SLAVE_EXISTS" = "None" ]; then
     echo "Slave IP address = " $SLAVE_ADDRESS
     echo
     echo "Waiting for slave init to complete..."
-    ssh ubuntu@$SLAVE_ADDRESS tail -f ~/aws-init.log | sed '/Slave initialization complete./ q'
+    ssh -o "StrictHostKeyChecking no" ubuntu@$SLAVE_ADDRESS tail -f /home/ubuntu/aws-init.log | sed '/Slave initialization complete./ q'
     echo "Commiting slave instance to image..." 
     aws ec2 create-image --instance-id $SLAVE_ID --name="SLAVE_AMI" --output text
     echo "Waiting for image (this will take time)..."
