71fb0e15e786a2990b4121777415b928f352e4ee
David Cako <kingcako@gmail.com>
2016-07-29 15:27:22 -0600

    until

--- ec2cerise.sh
+++ ec2cerise.sh
@@ -62,10 +62,9 @@ SLAVE_EXISTS=$(aws ec2 describe-images --filters "Name=owner-id,Values=$(aws sts
     echo
     echo "Waiting for instance up..."
     aws ec2 wait instance-running --instance-ids $SLAVE_ID  
-    ssh -o "StrictHostKeyChecking no" ubuntu@$SLAVE_ADDRESS "ls /home/ubuntu/aws-init.log"
-    while [ $? != 0 ]; do
-        echo $?
-        ssh -o "StrictHostKeyChecking no" ubuntu@$SLAVE_ADDRESS "ls /home/ubuntu/aws-init.log"
+    
+    until ssh -o "StrictHostKeyChecking no" ubuntu@$SLAVE_ADDRESS "ls /home/ubuntu/aws-init.log"
+        sleep 2
     done 
     echo "Tailing log file..."
     grep -q 'Slave initialization complete.' <(ssh -o "StrictHostKeyChecking no" ubuntu@$SLAVE_ADDRESS "tail -f /home/ubuntu/aws-init.log" | tee /dev/tty)
