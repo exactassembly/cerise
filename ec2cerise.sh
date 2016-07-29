301893e4d523b005456a38f97032cedac16360fa
David Cako <kingcako@gmail.com>
2016-07-29 14:11:18 -0600

    try kill $$

--- ec2cerise.sh
+++ ec2cerise.sh
@@ -64,7 +64,7 @@ SLAVE_EXISTS=$(aws ec2 describe-images --filters "Name=owner-id,Values=$(aws sts
     aws ec2 wait instance-running --instance-ids $SLAVE_ID  
     sleep 15
     echo "Tailing log file..."
-    ssh -o "StrictHostKeyChecking no" ubuntu@$SLAVE_ADDRESS "tail -f /home/ubuntu/aws-init.log" | sed '/Slave initialization complete./ q'
+    ssh -o "StrictHostKeyChecking no" ubuntu@$SLAVE_ADDRESS "tail -f /home/ubuntu/aws-init.log" | sed '/Slave initialization complete./ q' && kill $$
     echo "Commiting slave instance to image..." 
     aws ec2 create-image --instance-id $SLAVE_ID --name="SLAVE_AMI" --output text
     rm slave-ec2-init-tmp
