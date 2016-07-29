91a0aafdda3385c22f936f275f9a3383fcda40eb
David Cako <kingcako@gmail.com>
2016-07-29 15:08:13 -0600

    ssh

--- ec2cerise.sh
+++ ec2cerise.sh
@@ -64,11 +64,8 @@ SLAVE_EXISTS=$(aws ec2 describe-images --filters "Name=owner-id,Values=$(aws sts
     aws ec2 wait instance-running --instance-ids $SLAVE_ID  
     sleep 15
     echo "Tailing log file..."
-    ssh -o "StrictHostKeyChecking no" ubuntu@$SLAVE_ADDRESS "tail -f /home/ubuntu/aws-init.log" | while read line [ $line != 'Slave initialization complete.' ]
-    do
-        echo $line
-    done
-    
+    grep -q 'Slave initialization complete.' <(ssh -o "StrictHostKeyChecking no" ubuntu@$SLAVE_ADDRESS "tail -f /home/ubuntu/aws-init.log" | tee /dev/tty)
+
     echo "Commiting slave instance to image..." 
     aws ec2 create-image --instance-id $SLAVE_ID --name="SLAVE_AMI" --output text
     rm slave-ec2-init-tmp
