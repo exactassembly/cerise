5c143a5d92e5685bb6c5e2d2edfe33b7f6ba8994
David Cako <kingcako@gmail.com>
2016-08-01 13:09:27 -0600

    cerise fully documented.  move towards separate config files

--- cerise.sh
+++ cerise.sh
@@ -8,21 +8,37 @@
 . conf/ec2-init.conf
 
 # verify ec2-init.conf
-if [ ! "$GIT_TOKEN" ] || [ ! "SLAVE_PASS" ] || [ ! "AWS_KEYPAIR" ]; then
+if [ ! "$GIT_TOKEN" ] || [ ! "SLAVE_PASS" ]; then
     echo "Missing variable(s) in conf/ec2.init.conf."
     exit 1
 fi 
 
-aws sts get-caller-identity --output table
+aws sts get-caller-identity --output table  # simple check for aws configuration
 
-if [ $? != 0 ]; then
+if [ $? != 0 ]; then    # exit on non-zero exit status
     exit 1
 else
     echo "Valid awscli configuration found!"
 fi
 
-cp ec2-init/master-ec2-init ./master-ec2-init-tmp
+# check for keypair from conf file
+if [ ! "$AWS_KEYPAIR" ]; then
+    echo "No keypair referenced in configuration file."
+    if [ "$(ls *.pem)" ]; then  # check for keypair in current directory
+        KP_ARRAY=($(ls *.pem))
+        AWS_KEYPAIR=${KP_ARRAY[0]}
+        echo "Using" $AWS_KEYPAIR
+    else
+        echo "Generating keypair..."    # else generate keypair with random name
+        KP_RANDOM=$RANDOM
+        aws ec2 create-key-pair --key-name cerise --query "KeyMaterial" --output text > $KP_RANDOM".pem"
+        AWS_KEYPAIR=$KP_RANDOM".pem"
+        echo "Using" $AWS_KEYPAIR        
+fi
 
+cp ec2-init/master-ec2-init ./master-ec2-init-tmp   # move barebones init to tmp file for staging
+
+# replace empty variable assignments
 sed -i "" -e "s/GIT_TOKEN=\"\"/GIT_TOKEN=\"$GIT_TOKEN\"/" master-ec2-init-tmp
 sed -i "" -e "s/SLAVE_PASS=\"\"/SLAVE_PASS=\"$SLAVE_PASS\"/" master-ec2-init-tmp  
 
@@ -39,15 +55,16 @@ echo "Master instance ID =" $MASTER_ID
 echo "Master IP address = " $MASTER_ADDRESS
 echo
 
-rm master-ec2-init-tmp 
+rm master-ec2-init-tmp
 
-echo "Checking for existing slave AMI..."
+echo "Checking for existing slave AMI..."   
+# returns "None" if slave does not exist
 SLAVE_EXISTS=$(aws ec2 describe-images --filters "Name=owner-id,Values=$(aws sts get-caller-identity --query Account --output text)" "Name=name,Values=SLAVE_AMI" --query Images[0].ImageId --output text)
 
 # begin spinup of slave with master IP address in-hand
 if [ "$SLAVE_EXISTS" = "None" ]; then 
-    cp ec2-init/slave-ec2-init ./slave-ec2-init-tmp
-
+    cp ec2-init/slave-ec2-init ./slave-ec2-init-tmp  
+    
     sed -i "" -e "s/GIT_TOKEN=\"\"/GIT_TOKEN=\"$GIT_TOKEN\"/" slave-ec2-init-tmp
     sed -i "" -e "s/MASTER_ADDRESS=\"\"/MASTER_ADDRESS=\"$MASTER_ADDRESS\"/" slave-ec2-init-tmp
     sed -i "" -e "s/SLAVE_PASS=\"\"/SLAVE_PASS=\"$SLAVE_PASS\"/" slave-ec2-init-tmp
@@ -61,14 +78,15 @@ if [ "$SLAVE_EXISTS" = "None" ]; then
     echo "Slave IP address = " $SLAVE_ADDRESS
     echo
     echo "Waiting for instance up..."
-    aws ec2 wait instance-running --instance-ids $SLAVE_ID  
+    aws ec2 wait instance-running --instance-ids $SLAVE_ID  # blocks until instance state = running
     echo "Polling slave until SSH up..."
     until ssh -o "StrictHostKeyChecking no" ubuntu@$SLAVE_ADDRESS "ls /home/ubuntu/aws-init.log"; do
         sleep 3
     done 
     echo "Tailing log file..."
+    # ssh + tail onto log file.  redirect to grep, blocking until completion.  output also tee'd to /dev/tty.
     grep -q 'Slave initialization complete.' <(ssh -o "StrictHostKeyChecking no" ubuntu@$SLAVE_ADDRESS "tail -f /home/ubuntu/aws-init.log" | tee /dev/tty)
-    echo "Commiting slave instance to image..." 
+    echo "Commiting slave instance to image..." # with dummy slave fully staged, commit to image for later use
     aws ec2 create-image --instance-id $SLAVE_ID --name="SLAVE_AMI" --output text
     rm slave-ec2-init-tmp
     echo "Waiting for image (this will take time)..."
