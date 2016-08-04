581628949dec7de1df2dfeab683a4c1de54a995a
David Cako <kingcako@gmail.com>
2016-08-04 13:32:45 -0600

    begin implementing flask-security

--- cerise.py
+++ cerise.py
@@ -1,16 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.mongoengine import MongoEngine
+from flask.ext.security import Security, MongoEngineUserDatastore, UserMixin, \
+    RoleMixin, login_required    
 from urllib.parse import urlparse
 import boto3
 app = Flask(__name__)
+app.config.from_envvar('CERISE_CONFIG')
 
-AWS_ACCESS_ID=""
-AWS_ACCESS_KEY=""
+db = MongoEngine(app)
+
+class User(db.Document, UserMixin):
+    email = db.StringField(max_length=255)
+    password = db.StringField(max_length=255)
+    active = db.BooleanField(default=True)
+
+user_datastore = MongoEngineUserDatastore(db, User)
+security = Security(app, user_datastore)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/spinup', methods=['POST'])
+@login_required
 def spinup():
     parse = urlparse(request.form['repo'])
     if parse['scheme'] and parse.['netloc']:
@@ -19,4 +32,7 @@ def spinup():
         GIT_TOKEN = request.form['token']
     if 
 
+@app.route('/buildlight')
+@login_required
+def buildlight():
 
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var masterAddress = "";
+
+
+
 var builders = $.get(masterAddress + "/builders")
 
 function updateTable(){
-  
-})
+
+});
 
 
 var interval = setInterval(updateTable, 5000);
