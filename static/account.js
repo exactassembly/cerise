367be7a391764392cd6b60910333ea74f3473f35
David Cako <kingcako@gmail.com>
2016-08-15 16:43:40 -0600

    success handler

--- static/account.js
+++ static/account.js
@@ -1,9 +1,17 @@
 $( "#addProject" ).click(function(){
     $( "#newProject" ).css('display', 'inline');
     $( "#addProject" ).css('display', 'none');
-})
+});
 
-$( "#project" ).submit(function( event ){
-    payload = $( this ).serializeArray();
-    $.post( "/account", { name: $.})
-})
+$( ".projectForm" ).submit(function( event ){
+    payload = {};
+    payload.name = $( this ).attr("id");
+    payload.gitrepo = $( this ).children(".gitRepo");
+    payload.steps = {};
+    $( this ).children(".steps").children("li").each(function(index, value){
+        payload.steps[index] = value;
+    })
+    $.post( "/account", payload, function(){
+        location.reload();
+    });
+});
