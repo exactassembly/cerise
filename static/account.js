3332eb2318428d0382852836f4a405c4fefe99f9
David Cako <kingcako@gmail.com>
2016-08-17 12:43:04 -0600

    testing steps

--- static/account.js
+++ static/account.js
@@ -3,14 +3,20 @@ $( "#addProject" ).click(function(){
     $( "#addProject" ).css('display', 'none');
 });
 
+$( "#addStep" ).click(funciton(){
+    var index = $( "#steps" ).children().length
+    index = index+1
+    $( "#steps" ).append(String.format('<input id="steps-{0} name="steps-{0}" type="text"></input>', index)
+})
+
 $( "#newProject" ).submit(function( event ){
     event.preventDefault();
-    payload = {};
-    payload.name = $( this ).children("#name");
-    payload.gitrepo = $( this ).children("#gitrepo");
-    payload.steps = {};
+    var payload = {};
+    var payload.name = $( this ).children("#name");
+    var payload.gitrepo = $( this ).children("#gitrepo");
+    var payload.steps = {};
     $( this ).children(".steps").children("li").each(function(index, value){
-        payload.steps[index] = value;
+        var payload.steps[index] = value;
     })
     $.post( "/account", payload, function(){
         location.reload();
