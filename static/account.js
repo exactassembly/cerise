9447e01617afb7909565048450155574d7576905
David Cako <kingcako@gmail.com>
2016-08-17 13:07:02 -0600

    spacing

--- static/account.js
+++ static/account.js
@@ -5,8 +5,8 @@ $( "#addProject" ).click(function(){
 
 $( "#addStep" ).click(function(){
     var index = $( "#steps" ).children().length;
-    stepID = "steps-" + index + "step"
-    workdirID = "steps-" + index + "workdir"
+    stepID = "steps-" + index + "-step"
+    workdirID = "steps-" + index + "-workdir"
     $( "#steps" ).append("<div>");
     $( "#steps" ).append('<input id="' + stepID + '" name="' + stepID + ' type="text"></input>');
     $( "#steps" ).append('<input id="' + workdirID + '" name="' + workdirID + ' type="text"></input>'); 
