72c72e6631507711eba3d7960c5a8efb6bf6fd74
David Cako <kingcako@gmail.com>
2016-09-27 10:14:03 -0600

    move init masters

--- app/app.py
+++ app/app.py
@@ -3,6 +3,13 @@ from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config.from_envvar('CERISE_CONFIG')
-
 db = MongoEngine(app)
 
+def init_masters():
+    for group in Group.objects:
+        if len(group.projects) > 0:
+            p = subprocess.Popen(['buildbot', 'start'], cwd=group.directory)
+            group.pid = p.pid
+            group.save()
+
+init_masters()
