d3097a5b54b71638b05553b77698fef501f4751c
David Cako <kingcako@gmail.com>
2016-10-03 11:35:36 -0600

    change silly data attributes

--- app/static/js/account.js
+++ app/static/js/account.js
@@ -1,20 +1,20 @@
 $( "div #view" ).html('<iframe src="/buildlight"></iframe>');
 
 $( "#addProject" ).click(function(){
-    var group = $(this).attr('group');        
+    var group = $(this).attr('data-group');        
     $( "div #view" ).html('<iframe src="/account/add?group=' + group + '"></iframe>');
 });
 
 $( "#addSub" ).click(function(){
-    var group = $(this).attr('group');            
-    var project = $(this).attr('project');    
+    var group = $(this).attr('data-group');            
+    var project = $(this).attr('data-project');    
     $( "div #view" ).html('<iframe src="/account/add?group=' + group + '&parent=' + project + '></iframe>');
 });
 
 $( ".project-btn" ).click(function(){
-    var group = $(this).attr('group');   
-    var project = $(this).attr('project');
-    var sub = $(this).attr('sub');
+    var group = $(this).attr('data-group');   
+    var project = $(this).attr('data-project');
+    var sub = $(this).attr('data-sub');
     if (sub){
         $( "div #view" ).html('<iframe src="/project?group=' + group + '&project=' + project + '&sub=' + sub + '></iframe>');
     }
